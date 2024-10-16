e1dfed1287d2b6749cef0aaf7baeb8430e603349
alvinadeeb <prosmbapin@gmail.com>
2024-10-16 10:46:22 +0700

    index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,147 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="assets/style.css">
+        <link rel="stylesheet" href="font-awesome/css/font-awesome.css">
+    </head>
+    <body>
+        <header>
+            <div class="nav container">
+                <i class="fa fa-menu" id="menu-icon"></i>
+                <a href="#" class="logo">YAMA<span>HA</span></a>
+                <ul class="navbar">
+                    <li><a href="#" class="active">Home</a></li>
+                    <li><a href="#">FEAtures</a></li>
+                    <li><a href="#">PRICE</a></li>
+
+                </ul>
+            </div>
+        </header>
+        <!--end header-->
+        <section class="home">
+            <div class="home-text">
+                <h1>YAMAHA<br> <span>SEMAKIN</span> DIDEPAN!</h1>
+                <p>Roda kehidupan berputar, namun hanya Yamaha yang mampu </p>
+                <p>mengiringinya dengan kecepatan dan keandalan yang tak tertandingi.</p>
+            </div>
+        </section>
+        <!--end home-->
+        <section class="cycle" id="cycle">
+            <div class="heading">
+                <h2>FEATURES</h2>
+                
+                
+            </div>
+            <div class="cycle-container container">
+                <div class="box">
+                    <img src="assets/image/head.png" alt="">
+                    
+                </div>
+                <div class="box">
+                    <img src="assets/image/kelap.jpg" alt="">
+                    
+                </div>
+                <div class="box">
+                    <img src="assets/image/tangki.jpg" alt="">
+                    
+                </div>
+                <div class="box">
+                    <img src="assets/image/bluecore.jpg" alt="">
+                   
+                </div>
+                <div class="box">
+                    <img src="assets/image/mess.jpg" alt="">
+                    
+                </div>
+                <div class="box">
+                    <img src="assets/image/abs.png" alt="">
+                </div>
+                <div class="box">
+                    <img src="assets/image/jok.png" alt="">
+                </div>
+                <div class="box">
+                    <img src="assets/image/cas.png" alt="">
+                </div>
+                <div class="box">
+                    <img src="assets/image/spedo.jpg" alt="">
+                </div>
+                <div class="box">
+                    <img src="assets/image/abs.png" alt="">
+                </div>
+                <div class="box">
+                    <img src="assets/image/stoplamp.png" alt="">
+                </div>
+                <div class="box">
+                    <img src="assets/image/sampign.png" alt="">
+                </div>
+                <div class="box">
+                    <img src="assets/image/joks.png" alt="">
+                </div>
+                <div class="box">
+                    <img src="assets/image/abs.png" alt="">
+                </div>
+                <div class="box">
+                    <img src="assets/image/shok.png" alt="">
+                    
+                </div>
+            </div>
+        </section>
+        <!--end cycle-->
+        <!--end About-->
+        <section class="parts" id="parts">
+            <div class="heading">
+                <h2>VARIANT & PRICE</h2>
+                
+            </div>
+            <div class="parts-container container">
+                <div class="box">
+                    <img src="assets/image/11.png" alt="">
+                    <h3>Connected/ABS</h3>
+                    <p>Black Doff</p>
+                    <span>Rp 30.960.000</span>
+                    <a href="#" class="btn">Pre-Order</a>
+                    
+                </div>
+                <div class="box">
+                    <img src="assets/image/22.png" alt="">
+                    <h3>Connected/ABS</h3>
+                    <p>Grey</p>
+                    <span>Rp 30.960.000</span>                 
+                    <a href="#" class="btn">Pre-Order</a>
+                    
+                </div>
+                <div class="box">
+                    <img src="assets/image/44.png" alt="">
+                    <h3>CyberCity</h3>
+                    <p>Cyber City Livery</p>
+                    <span>Rp 27.375.000</span>
+                    <a href="#" class="btn">Pre-Order</a>
+                </div>
+                <div class="box">
+                    <img src="assets/image/55.png" alt="">
+                    <h3>Standar</h3>
+                    <p>Silver Cyan</p>
+                    <span>Rp 27.175.000</span>
+                    <a href="#" class="btn">Pre-Order</a>
+                </div>
+                <div class="box">
+                    <img src="assets/image/77.png" alt="">
+                    <h3>Standar</h3>
+                    <p>Metallic Red</p>
+                    <span>Rp 27.175.000</span>
+                    <a href="#" class="btn">Pre-Order</a>
+                </div>
+                <div class="box">
+                    <img src="assets/image/88.png" alt="">
+                    <h3>Standar</h3>
+                    <p>Black Grey</p>
+                    <span>Rp 27.175.000</span>
+
+                    <a href="#" class="btn">Pre-Order</a>
+
+                </div>
+            </div>
+        </section>
+       
+        </div>
+    </body>
+</html>
