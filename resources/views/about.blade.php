b55b55b2941aec53d043bc7f744c6d3caa66626e
alvinadeeb <prosmbapin@gmail.com>
2024-10-23 11:28:55 +0700

    update guys

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+  @vite('resources/css/app.css')
   <style>
     body {
       background-image: linear-gradient(to right, #4e54c8, #8f94fb);
